67475afebe3415cfddecd0d1f964d9b72f769b0f
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-22 14:43:54 +0300

    AlertDialog

---
+++ alertdialog/src/main/java/com/pk4u/alertdialog/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.pk4u.alertdialog
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pk4u.alertdialog.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.level1Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel1Activity::class.java))
+        }
+
+        binding.level2Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel2Activity::class.java))
+        }
+
+        binding.exitButton.setOnClickListener {
+            finish()
+        }
+    }
+
+}
