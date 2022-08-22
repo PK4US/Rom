264b7bf704fee01a68f5d8042c23713e1b0b0f43
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-22 17:48:49 +0300

    DialogFragment

---
+++ dialogfragment/src/main/java/com/pk4u/dialogfragment/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.pk4u.dialogfragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pk4u.dialogfragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.level1Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel1Activity::class.java))
+        }
+
+        binding.level2Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel2Activity::class.java))
+        }
+
+        binding.exitButton.setOnClickListener {
+            finish()
+        }
+    }
+}
---
+++ dialogfragment/src/main/java/com/pk4u/dialogfragment/VolumeAdapter.kt
@@ -0,0 +1,45 @@
+package com.pk4u.dialogfragment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.pk4u.dialogfragment.databinding.ItemVolumeSingleChoiceBinding
+
+
+class VolumeAdapter(
+    private val values: List<Int>
+) : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val context = parent.context
+        val binding = convertView?.tag as ItemVolumeSingleChoiceBinding? ?:
+            ItemVolumeSingleChoiceBinding.inflate(LayoutInflater.from(context)).also {
+                it.root.tag = it
+            }
+
+        val volume = getItem(position)
+
+        binding.volumeValueTextView.text = context.getString(R.string.volume_description, volume)
+        binding.volumeValueProgressBar.progress = volume
+
+        return binding.root
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+    override fun getItem(position: Int): Int {
+        return values[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return values.size
+    }
+
+}
